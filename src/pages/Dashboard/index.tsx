67606ef1bc814bfda256f27cbb0bea10f7c3b425
Rafael Japyassu <rafa.japyassu@gmail.com>
2021-05-26 17:18:17 -0300

    desafio resolvido

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -28,6 +28,8 @@ const Dashboard: React.FC = () => {
   useEffect(() => {
     async function loadFoods(): Promise<void> {
       // TODO LOAD FOODS
+      const response = await api.get('/foods');
+      setFoods(response.data);
     }
 
     loadFoods();
@@ -38,6 +40,14 @@ const Dashboard: React.FC = () => {
   ): Promise<void> {
     try {
       // TODO ADD A NEW FOOD PLATE TO THE API
+      const data = {
+        ...food,
+        available: true,
+      };
+
+      console.log(data);
+      const response = await api.post('/foods', data);
+      setFoods(oldState => [...oldState, response.data]);
     } catch (err) {
       console.log(err);
     }
@@ -47,10 +57,34 @@ const Dashboard: React.FC = () => {
     food: Omit<IFoodPlate, 'id' | 'available'>,
   ): Promise<void> {
     // TODO UPDATE A FOOD PLATE ON THE API
+    try {
+      // TODO ADD A NEW FOOD PLATE TO THE API
+      const data = {
+        ...food,
+        available: editingFood.available,
+      };
+      const response = await api.put(`/foods/${editingFood.id}`, data);
+
+      const newList = foods.filter(f => f.id !== response.data.id);
+
+      newList.push(response.data);
+
+      setFoods(newList);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   async function handleDeleteFood(id: number): Promise<void> {
     // TODO DELETE A FOOD PLATE FROM THE API
+    try {
+      // TODO ADD A NEW FOOD PLATE TO THE API
+      await api.delete(`/foods/${id}`);
+      const newList = foods.filter(f => f.id !== id);
+      setFoods(newList);
+    } catch (err) {
+      console.log(err);
+    }
   }
 
   function toggleModal(): void {
@@ -63,6 +97,8 @@ const Dashboard: React.FC = () => {
 
   function handleEditFood(food: IFoodPlate): void {
     // TODO SET THE CURRENT EDITING FOOD ID IN THE STATE
+    setEditingFood(food);
+    toggleEditModal();
   }
 
   return (
